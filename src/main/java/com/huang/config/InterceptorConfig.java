d3df8fc0ffdcb0cf142e94f93ade8a9b68f8a866
Slone Wang <handsomeslone@outlook.com>
2023-04-25 13:02:46 +0800

    swagger

--- src/main/java/com/huang/config/InterceptorConfig.java
+++ src/main/java/com/huang/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import com.huang.config.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,8 +14,19 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor( jwtInterceptor())
                 .addPathPatterns("/**") //拦截所有请求，通过判断token是否合法来决定是否要登录
-                .excludePathPatterns("/login","/register","**/export","**/import", "/test");
+                .excludePathPatterns("/login","/register","**/export","**/import", "/test")
+                .excludePathPatterns("/swagger**/**", "/webjars/**", "/v3/**", "/doc.html");
+        //WebMvcConfigurer.super.addInterceptors(registry);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+        //WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
 
     @Bean
     public JwtInterceptor jwtInterceptor(){
